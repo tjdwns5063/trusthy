2bbf2c796ca6bc404b0b701610f3153b8e7dd3a4
seongjunkim <tjdwns5063@naver.com>
2026-01-04 15:02:15 +0900

    feat: ExposureScoreCalculator 구현

---
+++ src/main/java/com/seongjki/trusthy/domain/ExposureScoreCalculator.java
@@ -0,0 +1,28 @@
+package com.seongjki.trusthy.domain;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class ExposureScoreCalculator {
+
+    private static final Double FRESHNESS_WEIGHT = 0.7;
+
+    private static final Double COMMENT_WEIGHT = 0.3;
+
+    private static final Double HELPFUL_WEIGHT = 0.3;
+
+    private static final Double TRUST_WEIGHT = 0.7;
+
+    public Double calculate(Post post) {
+        long freshness = LocalDateTime.now().toEpochSecond(ZoneOffset.of("+9")) - post.getCreatedAt().toEpochSecond(ZoneOffset.of("+9"));
+        int commentCount = post.getCommentCount();
+        int helpfulSum = post.getHelpfulSum();
+        TrustAccount.TrustLevel trustLevel = post.getCreatorLevel();
+
+        return (freshness * FRESHNESS_WEIGHT) +
+                (commentCount * COMMENT_WEIGHT) +
+                (helpfulSum * HELPFUL_WEIGHT) +
+                (trustLevel.getWeight() * TRUST_WEIGHT);
+    }
+
+}
